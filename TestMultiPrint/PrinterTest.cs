fa23f2e31d2f9f34acb1fb357c8e8d4e5f2bff33
Azbul <nabiev.bulat@mail.ru>
2018-08-07 14:23:05 +0300

    1

---
+++ TestMultiPrint/PrinterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Printing;
+using System.Drawing.Printing; 
+
+namespace TestMultiPrint
+{
+    class PrinterTest
+    {
+        public void PrNames()
+        {
+            PrintQueueCollection myPrintQueues = new PrintQueueCollection();
+            PrintQueue pq = LocalPrintServer.GetDefaultPrintQueue();
+            Console.WriteLine(pq.Name);
+           /* foreach(PrintQueue pq in myPrintQueues)
+            {
+                Console.WriteLine(pq.Name);
+            }
+            */
+            Console.WriteLine("This is PSetting function");
+
+            foreach (string str in PrinterSettings.InstalledPrinters)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ TestMultiPrint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMultiPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*PrinterTest pt = new PrinterTest();
+            pt.PrNames();
+            Console.ReadKey();*/
+
+            try
+
+            {
+                RawPrinterHelper.SendFileToPrinter(@"C:\Users\Adam\Documents\toDo.pdf");
+                Console.ReadKey();
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
